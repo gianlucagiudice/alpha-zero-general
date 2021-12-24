796eba346468ebc4a864fbcaf791c16401443b29
gianlucagiudice <gianlu.giudice@gmail.com>
2021-12-24 22:33:38 +0100

    Connect4 human player vs random player

---
+++ src/connect4_humanVSrandom.py
@@ -0,0 +1,17 @@
+from src.connect4.Connect4Game import Connect4Game
+from src.connect4.Connect4Players import RandomConnect4Player, HumanConnect4Player
+from Arena import Arena
+
+
+def main():
+    connect4game = Connect4Game()
+    player1 = HumanConnect4Player(connect4game, 1)
+    player2 = RandomConnect4Player(connect4game, -1)
+
+    print("\t\t===== CONNECT 4 GAME=====")
+    arena = Arena(player1, player2, connect4game, display=True)
+
+    arena.playGame(show=True, verbose=True)
+
+
+main()
