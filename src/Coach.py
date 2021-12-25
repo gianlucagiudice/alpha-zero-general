7e7d4665df98bdaf17f45fea0959f62cc1e797bc
gianlucagiudice <gianlu.giudice@gmail.com>
2021-12-25 23:39:46 +0100

    Multiprocessing lambda function serialization fixed

--- src/Coach.py
+++ src/Coach.py
@@ -1,20 +1,27 @@
 import logging
 import os
 import sys
+
 from collections import deque
 from pickle import Pickler, Unpickler
 from random import shuffle
 
-import numpy as np
+from src.connect4.Connect4Players import MCTSConnect4Player
+
+from multiprocessing import Pool
+
+
 from tqdm import tqdm
 
-from Arena import Arena
-from MCTS import MCTS
+import numpy as np
+
+from src.Arena import Arena
+from src.MCTS import MCTS
 
 log = logging.getLogger(__name__)
 
 
-class Coach():
+class Coach:
     """
     This class executes the self-play + learning. It uses the functions defined
     in Game and NeuralNet. args are specified in main.py.
@@ -25,11 +32,11 @@ class Coach():
         self.nnet = nnet
         self.pnet = self.nnet.__class__(self.game)  # the competitor network
         self.args = args
-        self.mcts = MCTS(self.game, self.nnet, self.args)
         self.trainExamplesHistory = []  # history of examples from args.numItersForTrainExamplesHistory latest iterations
         self.skipFirstSelfPlay = False  # can be overriden in loadTrainExamples()
 
-    def executeEpisode(self):
+    @staticmethod
+    def executeEpisode(mcts: MCTS):
         """
         This function executes one episode of self-play, starting with player 1.
         As the game is played, each turn is added as a training example to
@@ -46,27 +53,27 @@ class Coach():
                            the player eventually won the game, else -1.
         """
         trainExamples = []
-        board = self.game.getInitBoard()
-        self.curPlayer = 1
+        board = mcts.game.getInitBoard()
+        curPlayer = 1
         episodeStep = 0
 
         while True:
             episodeStep += 1
-            canonicalBoard = self.game.getCanonicalForm(board, self.curPlayer)
-            temp = int(episodeStep < self.args.tempThreshold)
+            canonicalBoard = mcts.game.getCanonicalForm(board, curPlayer)
+            temp = int(episodeStep < mcts.args['tempThreshold'])
 
-            pi = self.mcts.getActionProb(canonicalBoard, temp=temp)
-            sym = self.game.getSymmetries(canonicalBoard, pi)
+            pi = mcts.getActionProb(canonicalBoard, temp=temp)
+            sym = mcts.game.getSymmetries(canonicalBoard, pi)
             for b, p in sym:
-                trainExamples.append([b, self.curPlayer, p, None])
+                trainExamples.append([b, curPlayer, p, None])
 
             action = np.random.choice(len(pi), p=pi)
-            board, self.curPlayer = self.game.getNextState(board, self.curPlayer, action)
+            board, curPlayer = mcts.game.getNextState(board, curPlayer, action)
 
-            r = self.game.getGameEnded(board, self.curPlayer)
+            r = mcts.game.getGameEnded(board, curPlayer)
 
             if r != 0:
-                return [(x[0], x[2], r * ((-1) ** (x[1] != self.curPlayer))) for x in trainExamples]
+                return [(x[0], x[2], r * ((-1) ** (x[1] != curPlayer))) for x in trainExamples]
 
     def learn(self):
         """
@@ -77,23 +84,28 @@ class Coach():
         only if it wins >= updateThreshold fraction of games.
         """
 
-        for i in range(1, self.args.numIters + 1):
+        for i in range(1, self.args['numIters'] + 1):
             # bookkeeping
             log.info(f'Starting Iter #{i} ...')
             # examples of the iteration
             if not self.skipFirstSelfPlay or i > 1:
-                iterationTrainExamples = deque([], maxlen=self.args.maxlenOfQueue)
+                iterationTrainExamples = deque([], maxlen=self.args['maxlenOfQueue'])
 
-                for _ in tqdm(range(self.args.numEps), desc="Self Play"):
-                    self.mcts = MCTS(self.game, self.nnet, self.args)  # reset search tree
-                    iterationTrainExamples += self.executeEpisode()
+                with Pool(self.args['nThreads']) as pool:
+                    episods = [MCTS(self.game, self.nnet, dict(self.args)) for _ in range(self.args['numEps'])]
+                    train_examples = list(tqdm(pool.imap(self.executeEpisode, episods), total=self.args['numEps']))
+
+                # Add train example
+                for example in train_examples:
+                    iterationTrainExamples += example
 
                 # save the iteration examples to the history 
                 self.trainExamplesHistory.append(iterationTrainExamples)
 
-            if len(self.trainExamplesHistory) > self.args.numItersForTrainExamplesHistory:
+            if len(self.trainExamplesHistory) > self.args['numItersForTrainExamplesHistory']:
                 log.warning(
-                    f"Removing the oldest entry in trainExamples. len(trainExamplesHistory) = {len(self.trainExamplesHistory)}")
+                    f"Removing the oldest entry in trainExamples. "
+                    f"len(trainExamplesHistory) = {len(self.trainExamplesHistory)}")
                 self.trainExamplesHistory.pop(0)
             # backup history to a file
             # NB! the examples were collected using the model from the previous iteration, so (i-1)  
@@ -106,32 +118,37 @@ class Coach():
             shuffle(trainExamples)
 
             # training new network, keeping a copy of the old one
-            self.nnet.save_checkpoint(folder=self.args.checkpoint, filename='temp.h5')
-            self.pnet.load_checkpoint(folder=self.args.checkpoint, filename='temp.h5')
+            self.nnet.save_checkpoint(folder=self.args['checkpoint'], filename='temp.h5')
+            self.pnet.load_checkpoint(folder=self.args['checkpoint'], filename='temp.h5')
             pmcts = MCTS(self.game, self.pnet, self.args)
 
             self.nnet.train(trainExamples)
             nmcts = MCTS(self.game, self.nnet, self.args)
 
+            # Create arena
             log.info('PITTING AGAINST PREVIOUS VERSION')
-            arena = Arena(lambda x: np.argmax(pmcts.getActionProb(x, temp=0)),
-                          lambda x: np.argmax(nmcts.getActionProb(x, temp=0)), self.game)
-            pwins, nwins, draws = arena.playGames(self.args.arenaCompare)
+            p1, p2 = MCTSConnect4Player(self.game, 1, pmcts), MCTSConnect4Player(self.game, -1, nmcts)
+            arena = Arena(p1.play, p2.play, self.game)
 
+            # Play multiple games
+            pwins, nwins, draws = arena.playGames(self.args['arenaCompare'])
+
+            # Print games info
             log.info('NEW/PREV WINS : %d / %d ; DRAWS : %d' % (nwins, pwins, draws))
-            if pwins + nwins == 0 or float(nwins) / (pwins + nwins) < self.args.updateThreshold:
+            if pwins + nwins == 0 or float(nwins) / (pwins + nwins) < self.args['updateThreshold']:
                 log.info('REJECTING NEW MODEL')
-                self.nnet.load_checkpoint(folder=self.args.checkpoint, filename='temp.h5')
+                self.nnet.load_checkpoint(folder=self.args['checkpoint'], filename='temp.h5')
             else:
                 log.info('ACCEPTING NEW MODEL')
-                self.nnet.save_checkpoint(folder=self.args.checkpoint, filename=self.getCheckpointFile(i))
-                self.nnet.save_checkpoint(folder=self.args.checkpoint, filename='best.h5')
+                self.nnet.save_checkpoint(folder=self.args['checkpoint'], filename=self.getCheckpointFile(i))
+                self.nnet.save_checkpoint(folder=self.args['checkpoint'], filename='best.h5')
 
-    def getCheckpointFile(self, iteration):
+    @staticmethod
+    def getCheckpointFile(iteration):
         return 'checkpoint_' + str(iteration) + '.h5'
 
     def saveTrainExamples(self, iteration):
-        folder = self.args.checkpoint
+        folder = self.args['checkpoint']
         if not os.path.exists(folder):
             os.makedirs(folder)
         filename = os.path.join(folder, self.getCheckpointFile(iteration) + ".examples")
@@ -140,7 +157,7 @@ class Coach():
         f.closed
 
     def loadTrainExamples(self):
-        modelFile = os.path.join(self.args.load_folder_file[0], self.args.load_folder_file[1])
+        modelFile = os.path.join(self.args['load_folder_file'][0], self.args['load_folder_file'][1])
         examplesFile = modelFile + ".examples"
         if not os.path.isfile(examplesFile):
             log.warning(f'File "{examplesFile}" with trainExamples not found!')
