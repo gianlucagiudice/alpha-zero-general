3338bac306bd1eeadfc726bffe9754f47ca8fcd2
gianlucagiudice <gianlu.giudice@gmail.com>
2021-12-25 23:34:07 +0100

    Remove useless function

--- src/utils.py
+++ src/utils.py
@@ -16,7 +16,3 @@ class AverageMeter(object):
         self.count += n
         self.avg = self.sum / self.count
 
-
-class dotdict(dict):
-    def __getattr__(self, name):
-        return self[name]
