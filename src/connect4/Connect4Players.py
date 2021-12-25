f14d599cb9f6b0ac1ee6603f5aeb6746d14f9e2d
gianlucagiudice <gianlu.giudice@gmail.com>
2021-12-25 23:31:49 +0100

    Monte carlo tree search player

--- src/connect4/Connect4Players.py
+++ src/connect4/Connect4Players.py
@@ -1,7 +1,7 @@
 import numpy as np
 from src.connect4.Connect4Game import Connect4Game
 from src.Player import Player
-
+from src.MCTS import MCTS
 
 class RandomConnect4Player(Player):
     def __init__(self, game: Connect4Game, player: int):
@@ -34,36 +34,12 @@ class HumanConnect4Player(Player):
         return move
 
 
-class OneStepLookaheadConnect4Player:
-    def __init__(self, game, verbose=True):
+class MCTSConnect4Player:
+    def __init__(self, game, player, mcts: MCTS, verbose=False):
         self.game = game
-        self.player_num = 1
+        self.player = player
+        self.mcts = mcts
         self.verbose = verbose
 
     def play(self, board):
-        valid_moves = self.game.getValidMoves(board, self.player_num)
-        win_move_set = set()
-        fallback_move_set = set()
-        stop_loss_move_set = set()
-        for move, valid in enumerate(valid_moves):
-            if not valid: continue
-            if self.player_num == self.game.getGameEnded(*self.game.getNextState(board, self.player_num, move)):
-                win_move_set.add(move)
-            if -self.player_num == self.game.getGameEnded(*self.game.getNextState(board, -self.player_num, move)):
-                stop_loss_move_set.add(move)
-            else:
-                fallback_move_set.add(move)
-
-        if len(win_move_set) > 0:
-            ret_move = np.random.choice(list(win_move_set))
-            if self.verbose: print('Playing winning action %s from %s' % (ret_move, win_move_set))
-        elif len(stop_loss_move_set) > 0:
-            ret_move = np.random.choice(list(stop_loss_move_set))
-            if self.verbose: print('Playing loss stopping action %s from %s' % (ret_move, stop_loss_move_set))
-        elif len(fallback_move_set) > 0:
-            ret_move = np.random.choice(list(fallback_move_set))
-            if self.verbose: print('Playing random action %s from %s' % (ret_move, fallback_move_set))
-        else:
-            raise Exception('No valid moves remaining: %s' % game.stringRepresentation(board))
-
-        return ret_move
+        return np.argmax(self.mcts.getActionProb(board, temp=0))
